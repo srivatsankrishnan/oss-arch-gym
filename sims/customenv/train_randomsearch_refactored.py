cdbdb83a22369e27cef3d81773b76d0b1a8683bc
aadyapipersenia04 <20d170002@iitb.ac.in>
2023-07-03 14:06:14 +0000

    refactored randomsearch training script

---
+++ sims/customenv/train_randomsearch_refactored.py
@@ -0,0 +1,159 @@
+from concurrent import futures
+import grpc
+import portpicker
+import sys
+import os
+
+
+from absl import flags
+from absl import app
+from absl import logging
+
+os.sys.path.insert(0, os.path.abspath('../../'))
+# from configs import arch_gym_configs
+# from arch_gym.envs.envHelpers import helpers
+
+import envlogger
+import numpy as np
+import pandas as pd
+
+
+
+from arch_gym.envs.custom_env import CustomEnv
+from vizier.service import clients
+from vizier.service import pyvizier as vz
+from vizier.service import vizier_server
+from vizier.service import vizier_service_pb2_grpc
+
+flags.DEFINE_string('workload', 'stream.stl', 'Which DRAMSys workload to run?')
+flags.DEFINE_integer('num_steps', 100, 'Number of training steps.')
+flags.DEFINE_integer('num_episodes', 2, 'Number of training episodes.')
+flags.DEFINE_string('traject_dir', 
+                    'random_walker_trajectories', 
+            'Directory to save the dataset.')
+flags.DEFINE_bool('use_envlogger', False, 'Use envlogger to log the data.')  
+flags.DEFINE_string('summary_dir', '.', 'Directory to save the summary.')
+flags.DEFINE_string('reward_formulation', 'power', 'Which reward formulation to use?')
+FLAGS = flags.FLAGS
+
+def log_fitness_to_csv(filename, fitness_dict):
+        df = pd.DataFrame([fitness_dict['reward']])
+        csvfile = os.path.join(filename, "fitness.csv")
+        df.to_csv(csvfile, index=False, header=False, mode='a')
+
+        # append to csv
+        df = pd.DataFrame([fitness_dict])
+        csvfile = os.path.join(filename, "trajectory.csv")
+        df.to_csv(csvfile, index=False, header=False, mode='a')
+
+def wrap_in_envlogger(env, envlogger_dir):
+    metadata = {
+        'agent_type': 'RandomWalker',
+        'num_steps': FLAGS.num_steps,
+        'env_type': type(env).__name__,
+    }
+    if FLAGS.use_envlogger:
+        logging.info('Wrapping environment with EnvironmentLogger...')
+        env = envlogger.EnvLogger(env,
+                                  data_directory=envlogger_dir,
+                                  max_episodes_per_file=1000,
+                                  metadata=metadata)
+        logging.info('Done wrapping environment with EnvironmentLogger.')
+        return env
+    else:
+        return env
+
+
+
+def main(_):
+
+    env = CustomEnv()
+    observation = env.reset()
+    fitness_hist = {}
+    problem = vz.ProblemStatement()
+    problem.search_space.select_root().add_int_param(name='num_cores', min_value = 1, max_value = 12)
+    problem.search_space.select_root().add_float_param(name='freq', min_value = 0.5, max_value = 3)
+    problem.search_space.select_root().add_categorical_param(name='mem_type', feasible_values =['DRAM', 'SRAM', 'Hybrid'])
+    problem.search_space.select_root().add_discrete_param(name='mem_size', feasible_values=[0, 32, 64, 128, 256, 512])
+
+    problem.metric_information.append(
+        vz.MetricInformation(
+            name='energy', goal=vz.ObjectiveMetricGoal.MINIMIZE))
+
+    problem.metric_information.append(
+        vz.MetricInformation(
+            name='area', goal=vz.ObjectiveMetricGoal.MINIMIZE))
+
+    problem.metric_information.append(
+        vz.MetricInformation(
+            name='latency', goal=vz.ObjectiveMetricGoal.MINIMIZE))
+
+
+
+    study_config = vz.StudyConfig.from_problem(problem)
+    study_config.algorithm = vz.Algorithm.RANDOM_SEARCH
+    # RandomDesigner(search_space=problem.search_space)
+
+    port = portpicker.pick_unused_port()
+    address = f'localhost:{port}'
+
+    # Setup server.
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=100))
+
+    # Setup Vizier Service.
+    servicer = vizier_server.VizierService()
+    vizier_service_pb2_grpc.add_VizierServiceServicer_to_server(servicer, server)
+    server.add_secure_port(address, grpc.local_server_credentials())
+
+    # Start the server.
+    server.start()
+
+    clients.environment_variables.service_endpoint = address  # Server address.
+    study = clients.Study.from_study_config(
+        study_config, owner='owner', study_id='example_study_id')
+
+     # experiment name 
+    exp_name = "_num_steps_" + str(FLAGS.num_steps) + "_num_episodes_" + str(FLAGS.num_episodes)
+
+    # append logs to base path
+    log_path = os.path.join(FLAGS.summary_dir, 'random_walker_logs', FLAGS.reward_formulation, exp_name)
+
+    # get the current working directory and append the exp name
+    traject_dir = os.path.join(FLAGS.summary_dir, FLAGS.traject_dir, FLAGS.reward_formulation, exp_name)
+
+    # check if log_path exists else create it
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    if FLAGS.use_envlogger:
+        if not os.path.exists(traject_dir):
+            os.makedirs(traject_dir)
+    env = wrap_in_envlogger(env, traject_dir)
+
+    for i in range(flags.FLAGS.num_episodes):
+        suggestions = study.suggest(count=flags.FLAGS.num_steps)
+        for suggestion in suggestions:
+            num_cores = float(suggestion.parameters['num_cores'])
+            freq = float(suggestion.parameters['freq'])
+            mem_type_dict = {'SRAM':0, 'DRAM':1, 'Hybrid':2}
+            mem_type = float(mem_type_dict[suggestion.parameters['mem_type']])
+            mem_size = float(suggestion.parameters['mem_size'])
+            action = {"num_cores":num_cores, "freq": freq, "mem_type":mem_type, "mem_size": mem_size}
+            print("Suggested Parameters for num_cores, freq, mem_type, mem_size are :", num_cores, freq, mem_type, mem_size)
+            obs, reward, done, info = (env.step(action))
+            fitness_hist['reward'] = reward
+            fitness_hist['action'] = action
+            fitness_hist['obs'] = obs
+            log_fitness_to_csv(log_path, fitness_hist)
+            print(obs)
+            final_measurement = vz.Measurement({'energy': obs[0], 'area': obs[1], 'latency': obs[2]})
+            suggestion.complete(final_measurement)
+
+
+    for optimal_trial in study.optimal_trials():
+        optimal_trial = optimal_trial.materialize()
+        print("Optimal Trial Suggestion and Objective:", optimal_trial.parameters,
+                optimal_trial.final_measurement)
+
+if __name__ == '__main__':
+   app.run(main)
