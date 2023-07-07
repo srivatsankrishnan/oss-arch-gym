d1c33ba4af6edb53a0770af5fd57ce89d582c225
pipersenia <20d170002@iitb.ac.in>
2023-07-07 16:43:39 +0530

    updated doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,10 +7,10 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../sims/CustomEnv'))
+sys.path.insert(0, os.path.abspath('../../sims/customenv'))
 sys.path.insert(0, os.path.abspath('../../arch_gym'))
 sys.path.insert(0, os.path.abspath('../../arch_gym/envs'))
-
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'OSS-Vizier'
 copyright = '2023, -'
