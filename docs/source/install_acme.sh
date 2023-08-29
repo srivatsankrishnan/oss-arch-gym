12df2777f6adc5308fee517099cddfe4c859f2fc
shreyasgrampurohit <shreyasg0512@gmail.com>
2023-08-29 22:33:47 +0530

    Added LD_LIBRARY_PATH

--- docs/source/install_acme.sh
+++ docs/source/install_acme.sh
@@ -3,6 +3,8 @@
 # Set execute permissions for the script itself
 chmod +x "$0"
 
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/$USERNAME/anaconda3/envs/arch-gym/lib/"
+
 # Change directory to the "acme" folder
 cd ../../acme
 
