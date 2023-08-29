049985d19674acd70e0b35a8a5b0232e5f445037
shreyasgrampurohit <shreyasg0512@gmail.com>
2023-08-29 13:10:56 +0530

    shell script

--- docs/source/install_acme.sh
+++ docs/source/install_acme.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Change directory to the "acme" folder
-cd ../../acme
-
 # Set execute permissions for the script itself
 chmod +x "$0"
+
+# Change directory to the "acme" folder
+cd ../../acme
 
 # Install required packages using pip
 pip install .[jax,tf,testing,envs]
