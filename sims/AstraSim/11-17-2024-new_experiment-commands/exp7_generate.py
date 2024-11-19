0d6de6626b92fc4b4df3becca3c63ef4538d33b8
jared-ni <milkteadj@gmail.com>
2024-11-19 02:32:57 -0500

    generate all experiments and commands

---
+++ sims/AstraSim/11-17-2024-new_experiment-commands/exp7_generate.py
@@ -0,0 +1,91 @@
+import yaml
+
+for workload in ["multi_modal"]:
+    # b) ga
+    steps_range = [1024, 10000]
+    num_agents_range = [32, 64]
+    prob_mut_range = [0.01, 0.05]
+    # Iterate through combinations and create YAML files
+    for step in steps_range:
+        for num_agents in num_agents_range:
+            for prob in prob_mut_range:
+                cur_yml = {
+                    "ALGORITHM": "ga",
+                    "STEPS": step,
+                    "NUM_AGENTS": num_agents,
+                    "PROB_MUT": prob,
+                    "REWARD": "both",
+                    "KNOBS": f"astrasim_220_example/knobs_7.py",
+                    "NETWORK": f"astrasim_220_example/network_input_7.yml",
+                    "SYSTEM": f"astrasim_220_example/system_input_7.json",
+                    "WORKLOAD": ["astrasim_220_example/workload_cfg_7_gpt3_13b.json", "astrasim_220_example/workload_cfg_7_gpt3_175b.json", "astrasim_220_example/workload_cfg_7_vit_base.json", "astrasim_220_example/workload_cfg_7_vit_large.json"]
+                }
+                
+                prob_str = ""
+                if prob == 0.01:
+                    prob_str = '01'
+                elif prob == 0.05:
+                    prob_str = '05'
+                file_name = f"exp7/experiment7b_{step}_{num_agents}_{prob_str}_{workload}.yml"
+                
+                with open(file_name, "w") as file:
+                    yaml.dump(cur_yml, file)
+                    
+                print(file_name)
+
+    # c) aco
+    steps_range = [1024, 10000]
+    ant_count = [8, 16]
+    greediness_range = [0.25, 0.5, 0.75]
+    evaporation_range = [0.25, 0.5, 0.75]
+
+    # Iterate through combinations and create YAML files
+    for step in steps_range:
+        for ant in ant_count:
+            for greed in greediness_range:
+                for eva in evaporation_range:
+                    cur_yml = {
+                        "ALGORITHM": "aco",
+                        "STEPS": step,
+                        "ANT_COUNT": ant,
+                        "GREEDINESS": greed,
+                        "EVAPORATION": eva,
+                        "REWARD": "both",
+                        "KNOBS": f"astrasim_220_example/knobs_7.py",
+                        "NETWORK": f"astrasim_220_example/network_input_7.yml",
+                        "SYSTEM": f"astrasim_220_example/system_input_7.json",
+                        "WORKLOAD": ["astrasim_220_example/workload_cfg_7_gpt3_13b.json", "astrasim_220_example/workload_cfg_7_gpt3_175b.json", "astrasim_220_example/workload_cfg_7_vit_base.json", "astrasim_220_example/workload_cfg_7_vit_large.json"]
+                    }
+                    greed_str = str(int(100 * greed))
+                    eva_str = str(int(100 * eva))
+                    file_name = f"exp7/experiment7c_{step}_{ant}_{greed_str}_{eva_str}_{workload}.yml"
+                    
+                    with open(file_name, "w") as file:
+                        yaml.dump(cur_yml, file)
+                        
+                    print(file_name)
+
+
+    # d) bo
+    steps_range = [1024, 5000]
+    rand_state = [1, 2, 3, 4]
+    # Iterate through combinations and create YAML files
+    for step in steps_range:
+        for rand in rand_state:
+            cur_yml = {
+                "ALGORITHM": "bo",
+                "STEPS": step,
+                "RAND_STATE_BO": rand,
+                "REWARD": "both",
+                "KNOBS": f"astrasim_220_example/knobs_7.py",
+                "NETWORK": f"astrasim_220_example/network_input_7.yml",
+                "SYSTEM": f"astrasim_220_example/system_input_7.json",
+                "WORKLOAD": ["astrasim_220_example/workload_cfg_7_gpt3_13b.json", "astrasim_220_example/workload_cfg_7_gpt3_175b.json", "astrasim_220_example/workload_cfg_7_vit_base.json", "astrasim_220_example/workload_cfg_7_vit_large.json"]
+            }
+            
+            file_name = f"exp7/experiment7d_{step}_{rand}_{workload}.yml"
+            
+            with open(file_name, "w") as file:
+                yaml.dump(cur_yml, file)
+                
+            print(file_name)
