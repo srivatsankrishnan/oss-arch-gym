a1c1b157c287e63b1e9045ef6203256c6637b1f5
AditiR-42 <aditisanram@gmail.com>
2025-02-19 14:27:44 -0500

    add three more experiments

---
+++ sims/AstraSim/astrasim_220_example/knobs_1_extended.py
@@ -0,0 +1,22 @@
+# MUST BE HYPHENATED
+SYSTEM_KNOBS = {
+}
+
+# MUST BE HYPHENATED
+NETWORK_KNOBS = {
+}
+
+# MUST USE UNDERSCORES
+WORKLOAD_KNOBS = {
+    'dp': ({1, 2, 4, 8, 16, 32, 64, 128, 256, 512}, 'N/A'),
+    'pp': ({1, 2, 4}, 'N/A'),
+    'sp': ({1, 2, 4, 8, 16, 32, 64, 128, 256, 512}, 'N/A'),
+	'weight_sharded': ((0, 1, 1), 'N/A'),
+    'batch': ({1024, 2048, 4069}, 'N/A'),
+}
+
+DERIVED_KNOBS = []
+
+CONSTRAINTS = ["product network npus-count == num workload num_npus", "mult workload dp workload sp workload pp <= num workload num_npus"]
+
+# DESIGN SPACE = 600
